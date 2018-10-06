416d4bf015a27b77f925c360f3df3be329521c54
Lucas Balzer <info@somadg.com>
2018-10-06 10:40:38 -0700

    Change button UI/UX
    Update visual styling and make a single button work for both show and hide

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,24 @@
 (function($) {
 
-  $("#sidebar-toggler .hide").click(function() {
-    $("#sidebar-toggler .show").removeClass("active");
-    $(this).addClass("active");
-    $("#ct-viewport-container").css("margin-left", "0");
-    $("#ct-viewport-container").css("width", "100%");
-    $("#oxygen-sidebar, .oxygen-add-section-library-flyout-panel").css(
-      "display",
-      "none"
-    );
-  });
-
-  $("#sidebar-toggler .show").click(function() {
-      $("#sidebar-toggler .hide").removeClass("active");
-      $(this).addClass("active");
-    $("#ct-viewport-container").css("margin-left", "300px");
-      $("#ct-viewport-container").css("width", "calc(100% - 300px)");
-    $("#oxygen-sidebar, .oxygen-add-section-library-flyout-panel").css(
-      "display",
-      "block"
-    );
+  $("#sidebar-toggler button").click(function() {
+    $(this).toggleClass('hide show').find('i').toggleClass('fa-eye fa-eye-slash');
+    
+    if ($(this).hasClass('show')){
+        $("#ct-viewport-container").css("margin-left", "0");
+        $("#ct-viewport-container").css("width", "100%");
+        $("#oxygen-sidebar, .oxygen-add-section-library-flyout-panel").css(
+          "display",
+          "none"
+        );
+    }
+    else {
+        $("#ct-viewport-container").css("margin-left", "300px");
+        $("#ct-viewport-container").css("width", "calc(100% - 300px)");
+        $("#oxygen-sidebar, .oxygen-add-section-library-flyout-panel").css(
+        "display",
+        "block"
+        );
+    }
   });
 
 })(jQuery);
